c93d9f502151a3acc437ef46694e162ee3c458c4
codewmanas <manas.kolaskar@somaiya.edu>
2025-03-16 14:21:28 +0530

    design theme setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_certification/app_view.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Project',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        body: const Center(
-          child: Text('Hello, World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const MyAppView());
 }
